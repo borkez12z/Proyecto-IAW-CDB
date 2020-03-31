3befe7967233d2fa940fca7c157562b248793fa9
Rafael Bohorquez Gomez <rafaelgomezbohorquez2000@gmail.com>
2020-03-31 13:42:09 +0200

    Primer Commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>CDB ARAHAL</title>
+    </head>
+    <body>
+        <form action="" method="post">
+            <h1>CDB ARAHAL</h1>
+            <hr></hr>
+            <input type="submit" name="inscribir" value="INSCRIBIR JUGADOR"/>
+            <input type="submit" name="actualizar" value="ACTUALIZAR ESTADISTICAS JUGADOR"/>
+            <input type="submit" name="borrar" value="BORRAR JUGADOR"/>
+            <input type="submit" name="buscar" value="BUSCAR JUGADOR"/>
+        </form>
+    <body>
+</html>
